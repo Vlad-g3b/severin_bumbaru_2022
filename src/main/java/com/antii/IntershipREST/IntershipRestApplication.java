f7c74450c6825b15d4217593c18b28479ba51f94
Vlad-g3b <vladut_panaite@yahoo.com>
2022-05-18 13:11:39 +0300

    first

---
+++ src/main/java/com/antii/IntershipREST/IntershipRestApplication.java
@@ -0,0 +1,13 @@
+package com.antii.IntershipREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antii/IntershipREST/IntershipRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antii.IntershipREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershipRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
